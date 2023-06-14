e18859aef8f78ceb09fc7bec74f00fd382fecc4c
lipika.manglaa <lipika.mangla.web@gmail.com>
2023-06-14 22:31:18 +1000

    generate passowrd

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # passwordGenerator
+
+# 03 JavaScript: Password Generator
+
+
+
+This application emphasizes the use of Javascript to generate a random, secure password for the user. 
+
+This project has been deployed to GitHub Pages. To get this project up and running, you can follow the deployment link. Or, download the sources files to use this as a template.
+
+
+### Prerequisites
+
+To install this application, you will need a text editor. I recommend Visual Studio Code. 
+
+### Installing
+
+To install this code, download the zip file, or use GitHub's guidelines to clone the repository. 
+
+### This project has the following features: 
+* A generate button
+    * This will send the user a series of prompts and confirms
+    * After user data is collected, a random password will be generated using Javascript
+
+
+* A Textarea
+    * This textarea will display the users password once it is generated
+
+
+* A Copy button 
+    * This will copy the users password to the user's devices clipboard
+
+## User Story
+
+
+* The user will be prompted to choose from the following password criteria: 8 and 128 characters
+* The user will recieve a confirm for: 
+    * Password containing special characters, numbers, and uppercase
+    * Either 4 variables individual ones, or 3 with toLowerCase to Uppercase conversion
+    * This will need to randomly generate a selection or randomly select array data, so math.random and math.floor will need to be used.     
+* The application should validate user input and ensure that at least one character type is selected.
+   
+    * Event listener will determine the password output with function to populate the value into the test area.
+
+
+ 
+
+## Acceptance Criteria
+## Features:
+* Variable declaration area 
+* An event listener (onclick) called generatePassword
+    * This will prompt the user for input between 8-128
+    * This variable is changed to an interger using ParseInt()
+    * This will validate that the input is a number within range, or is a number
+    * This then uses the input to determine the types or letters of characters used, using an if statement
+    * This then assigns values to the variables using arrays for character, number or alphabet
+* Another variable is created to concatenate the above variables
+* A for loop will loop through the enter prompt until it reaches the number entered by user. 
+* A password variable takes the value from the for loop, and converts it to a string. 
+* The string value then populates into the text area for the user using a UserInput function.
+
+
+
---
+++ script.js
@@ -0,0 +1,161 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+//upperCaseLetters
+let upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let upperLetters = upperCaseLetters.split('');
+
+//lowerCaseLetters
+let lowercaseLetters = upperCaseLetters.toLowerCase()
+let lowerLetters = lowercaseLetters.split('');
+
+//Numeric characters
+let numberData = "0123456789";
+let numberLetter = Array.from(String(numberData), Number);
+
+
+//special Characters
+let specialSymbol = "!@#$%^&*()_+~|}{[]><.-/\=";
+let specialSymbolConvert = specialSymbol.split('');
+
+let password = document.querySelector("#password");
+
+//add event listener for generate password in textbox
+generateBtn.addEventListener('click', () => {
+
+  
+  password.value = generatePassword()
+
+})
+
+
+let passwordGenerate = [];
+let passwordString;
+let passwordChoose;
+let userInput, userInputNumber, userInputCharacter, userInputLowercase, userInputUppercase, selection;
+function generatePassword() {
+  //Get input from User
+  userInput = parseInt(prompt("How many characters would you like your password to contain?"))
+
+  //if input is empty,alert message show
+  if (!userInput) {
+    alert("Please enter value between 8 to 128")
+  }
+  else if (userInput < 8 || userInput > 128) {
+    alert("please select between 8 and 128 characters")
+  } else {
+    userInputNumber = confirm("click ok to confirm including numeric characters");
+    userInputCharacter = confirm("click ok to confirm including special characters?");
+    userInputUppercase = confirm("click ok to confirm including uppercase letters");
+    userInputLowercase = confirm("click ok to confirm including lowerCase letters");
+  }
+
+  // Else if for selecting option if user does not select any input
+  if (userInputNumber == "" && userInputCharacter == "" && userInputLowercase == "" && userInputUppercase == "") {
+    selection = alert("You must aleast choose any selection!");
+
+  }
+  else if (userInputNumber && userInputCharacter && userInputLowercase && userInputUppercase) {
+    selection = numberLetter.concat(specialSymbolConvert, lowerLetters, upperLetters);
+
+  }
+  //Three input selection start here
+  //selection three input except upperCase
+  else if (userInputNumber && userInputCharacter && userInputLowercase) {
+    selection = numberLetter.concat(specialSymbolConvert, lowerLetters);
+  }
+  //selection three input except lowerCase
+  else if (userInputNumber && userInputCharacter && userInputUppercase) {
+    selection = numberLetter.concat(specialSymbolConvert, upperLetters);
+
+  }
+  //selection three input except special character
+  else if (userInputNumber && userInputLowercase && userInputUppercase) {
+    selection = numberLetter.concat(lowerLetters, upperLetters);
+
+  }
+
+  //selection three input except numbers
+  else if (userInputLowercase && userInputCharacter && userInputUppercase) {
+    selection = lowerLetters.concat(specialSymbolConvert, upperLetters);
+
+  }
+  //Three input selction ends here
+
+  // Two input selction start here
+  //selction two input except lowerCase and upperCase
+  else if (userInputNumber && userInputCharacter) {
+    selection = numberLetter.concat(specialSymbolConvert);
+  }
+  //selction two input except lowerCase and Number
+  else if (userInputCharacter && userInputUppercase) {
+    selection = specialSymbolConvert.concat(upperLetters);
+
+  }
+  //selction two input except lowerCase and special characters
+  else if (userInputNumber && userInputUppercase) {
+    selection = numberLetter.concat(upperLetters);
+
+  }
+  //selection two input except uppercase and special characters
+  else if (userInputNumber && userInputLowercase) {
+    selection = numberLetter.concat(lowerLetters);
+
+  }
+  //selection two input except uppercase and Numbers
+  else if (userInputCharacter && userInputLowercase) {
+    selection = specialSymbolConvert.concat(lowerLetters);
+
+  }
+  //selction two input except Numbers and special characters
+  else if (userInputLowercase && userInputUppercase) {
+    selection = lowerLetters.concat(upperLetters);
+
+  }
+  // two input selection ends here
+
+  // Single input from user start here
+
+  else if (userInputLowercase) {
+    selection = lowerLetters;
+
+  }
+  else if (userInputCharacter) {
+    selection = specialSymbolConvert;
+
+  }
+  else if (userInputNumber) {
+    selection = numberLetter;
+
+  }
+  else if (userInputUppercase) {
+    selection = upperLetters;
+
+  }
+
+  // Single input from user ends here
+
+  // password variable is an array placeholder for user generated amount of length
+
+  // Start random selection variables:
+  // Random selection for all variables: 
+  for (var i = 0; i < userInput; i++) {
+    passwordChoose = selection[Math.floor(Math.random() * selection.length)];
+    passwordGenerate.push(passwordChoose);
+  }
+  
+  // This joins the password array and converts it to a string
+
+  passwordString = passwordGenerate.join("");
+
+  return passwordString;
+}
+
+
+
+
+
+
+
+
